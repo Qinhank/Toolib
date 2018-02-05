16b8a04c9abf272ffd88c632511ae4da43e1ca1f
Home <hank.qin1994@gmail.com>
2018-02-05 22:33:59 +0800

    debug onlyYmd

--- src/onlyYmd.js
+++ src/onlyYmd.js
@@ -4,7 +4,7 @@
  * @return {[type]} [返回格式：2018-01-02]
  */
 Date.prototype.onlyYmd = function(){
-	var month = this.getMonth() + 1 <= 10 ? '0' + (this.getMonth() + 1) : this.getMonth() + 1;
-	var date = this.getDate() + 1 <= 10 ? '0' + this.getDate() : this.getDate();
+	var month = this.getMonth() + 1 < 10 ? '0' + (this.getMonth() + 1) : this.getMonth() + 1;
+	var date = this.getDate() + 1 < 10 ? '0' + this.getDate() : this.getDate();
 	return this.getFullYear() + "-" + month + "-" + date;
 }
